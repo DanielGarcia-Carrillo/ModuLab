07696a2eb589074458e64c344a1951368c940063
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2014-02-04 17:17:48 -0600

    added submit procedure form that can be accessed from http://localhost:5000/submit_procedure, it doesn't really do anything right now but it exposes some new tech that we're using

--- ModuLab.py
+++ ModuLab.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template
-import json
+from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# TODO: sqlite database setup, we should move over to postgresql later
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
+@app.route('/', methods=['GET'])
+def landing_page():
+    return render_template('base.html')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/submit_procedure', methods=['GET', 'POST'])
 def hello_world():
-    return render_template('protocol_submission.html')
+    if request.method == 'POST':
+        # TODO: take the form parameters and move them into the database
+        proc_name = request.form['name']
+        return None
+    else:
+        return render_template('protocol_form.html')
+
+@app.errorhandler(500)
+def internal_error(error):
+    print error
+    return "500 error"
 
 if __name__ == '__main__':
     app.run()
